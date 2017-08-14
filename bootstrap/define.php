1b2c0dbaadaddd8d04ebcc52622791291dc4316a
kakuilan <kakuilan@163.com>
2017-08-14 23:56:19 +0800

    define 常量定义

---
+++ bootstrap/define.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2017 LKK/lianq.net All rights reserved
+ * User: kakuilan@163.com
+ * Date: 2017/8/14
+ * Time: 23:54
+ * Desc: -常量定义
+ */
+
+
+define('DS', str_replace('\\', '/', DIRECTORY_SEPARATOR));
+define('PS', PATH_SEPARATOR);
+define('PHPEXT', '.php');
+
+define('KSERVER_NAME', 'KSS');
+define('KSERVER_VERS', '0.0.0.1');
+
+
+
