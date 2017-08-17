bedca7cff78cbc3c7592193934c86b27d1dcdf2e
kakuilan <kakuilan@163.com>
2017-08-17 22:08:54 +0800

    define

--- bootstrap/define.php
+++ bootstrap/define.php
@@ -8,12 +8,33 @@
  */
 
 
+define('STARTTIME', microtime(true));
 define('DS', str_replace('\\', '/', DIRECTORY_SEPARATOR));
 define('PS', PATH_SEPARATOR);
-define('PHPEXT', '.php');
 
 define('KSERVER_NAME', 'KSS');
 define('KSERVER_VERS', '0.0.0.1');
 
+define('ROOTDIR', str_replace('\\', '/', dirname(__DIR__)) . DS ); //根目录
+define('BINDIR',    ROOTDIR .'bin'          . DS ); //执行目录
+define('WWWDIR',    ROOTDIR .'public'       . DS ); //WEB入口目录
+define('BOOTDIR',   ROOTDIR .'bootstrap'    . DS );
+define('APPSDIR',   ROOTDIR .'apps'         . DS );
+define('CONFDIR',   ROOTDIR .'config'       . DS );
+define('DATADIR',   ROOTDIR .'data'         . DS );
+define('KENGDIR',   ROOTDIR .'kengine'      . DS );
+define('RUNTDIR',   ROOTDIR .'runtime'      . DS );
+define('SERVDIR',   ROOTDIR .'server'       . DS );
+define('TESTDIR',   ROOTDIR .'tests'        . DS );
 
+define('LOGDIR',    RUNTDIR .'logs'         . DS );
+define('PIDDIR',    RUNTDIR .'pids'         . DS );
+define('MODULDIR',  APPSDIR .'modules'      . DS ); //应用模块目录
+define('CTASKDIR',  MODULDIR .'cli/tasks'   . DS ); //CLI任务目录
+define('MODELDIR',  APPSDIR .'models'       . DS ); //数据模型类目录
+define('VIEWDIR',   APPSDIR .'views'        . DS ); //视图目录
+define('UPLODIR',   WWWDIR .'upload'        . DS ); //上传目录
+
+define('REDIS_CACHE_DB', 0); //用于存储CACHE的redis库
+define('REDIS_SESSION_DB', 1); //用于存储SESSION的redis库
 
