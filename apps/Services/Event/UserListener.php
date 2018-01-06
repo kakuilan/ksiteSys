4a827ae2380f46d69321f5caa368348c9fb30dd1
kakuilan <kakuilan@163.com>
2018-01-06 23:33:11 +0800

    add UserListener

---
+++ apps/Services/Event/UserListener.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) 2018 LKK/lianq.net All rights reserved
+ * User: kakuilan@163.com
+ * Date: 2018/1/6
+ * Time: 23:32
+ * Desc: -用户服务监听类
+ */
+
+
+namespace Apps\Services\Event;
+
+class UserListener extends ListenerBase {
+
+
+}
