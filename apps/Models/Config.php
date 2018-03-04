d4db3265268234f10273725afcf3220413fb299a
kakuilan <kakuilan@163.com>
2018-03-04 20:34:17 +0800

    add Config model

---
+++ apps/Models/Config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright (c) 2018 LKK/lianq.net All rights reserved
+ * User: kakuilan@163.com
+ * Date: 2018/3/4
+ * Time: 20:33
+ * Desc: -数据表模型 配置表config
+ */
+
+
+namespace Apps\Models;
+
+class Config extends BaseModel {
+
+    public function initialize() {
+        parent::initialize();
+    }
+
+}
