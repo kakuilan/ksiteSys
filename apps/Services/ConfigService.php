f7a8c871a6d6200081155c5d263f0753b9547af2
kakuilan <kakuilan@163.com>
2018-03-04 22:26:29 +0800

    add ConfigService

---
+++ apps/Services/ConfigService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright (c) 2018 LKK/lianq.net All rights reserved
+ * User: kakuilan@163.com
+ * Date: 2018/3/4
+ * Time: 22:24
+ * Desc: -配置服务类
+ */
+
+
+namespace Apps\Services;
+
+class ConfigService extends ServiceBase {
+
+
+}
