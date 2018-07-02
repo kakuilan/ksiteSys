d7811f8cb3fa9176e230f8c4e382497f8c48a68a
kakuilan <kakuilan@163.com>
2018-07-02 21:08:31 +0800

    add CrontabController

---
+++ apps/Modules/Manage/Controllers/CrontabController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doubo
+ * Date: 2018/7/2
+ * Time: 21:03
+ * Desc:
+ */
+
+
+namespace Apps\Modules\Manage\Controllers;
+
+use Apps\Modules\Manage\Controller;
+use Apps\Models\Config;
+use Apps\Models\UserBase;
+use Apps\Services\UserService;
+use Lkk\Helpers\ArrayHelper;
+use Lkk\Helpers\StringHelper;
+use Lkk\Helpers\ValidateHelper;
+
+/**
+ * Class 后台定时任务控制器
+ * @package Apps\Modules\Manage\Controllers
+ */
+class CrontabController extends Controller {
+
+    public function initialize () {
+        parent::initialize();
+
+        $this->setHeaderSeo('管理后台', '关键词', '描述');
+
+        //视图变量
+        $this->view->setVars([
+            'headerSeo' => $this->headerSeo,
+        ]);
+
+    }
+
+
+}
