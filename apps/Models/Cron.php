a02b4d892b64c33c86c1d0ce179d84097b5d4d24
kakuilan <kakuilan@163.com>
2018-07-02 21:12:26 +0800

    add 定时任务数据模型

---
+++ apps/Models/Cron.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kakuilan@163.com
+ * Date: 2018/7/2
+ * Time: 21:10
+ * Desc: -数据表模型 定时任务表cron
+ */
+
+namespace Apps\Models;
+
+class Cron extends BaseModel {
+
+    public function initialize() {
+        parent::initialize();
+    }
+
+}
---
+++ apps/Models/CronLog.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kakuilan@163.com
+ * Date: 2018/7/2
+ * Time: 21:11
+ * Desc: -数据表模型 定时任务日志表cron_log
+ */
+
+namespace Apps\Models;
+
+class CronLog extends BaseModel {
+
+    public function initialize() {
+        parent::initialize();
+    }
+
+}
