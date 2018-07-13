59fc419337f1a6073c5e0a74a0da5391af302734
kakuilan <kakuilan@163.com>
2018-07-13 09:58:23 +0800

    add ThumbService

---
+++ apps/Services/ThumbService.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doubo
+ * Date: 2018/7/13
+ * Time: 9:57
+ * Desc: 缩略图服务类
+ */
+
+
+namespace Apps\Services;
+
+use Apps\Models\Attach;
+use Apps\Models\UserBase;
+use Lkk\Helpers\ArrayHelper;
+use Lkk\Helpers\ValidateHelper;
+
+class ThumbService extends ServiceBase {
+
+
+}
